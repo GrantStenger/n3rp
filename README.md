a4eba628047ce3988e754f20e919127f020b7cb1
Grant Stenger <gstenger98@gmail.com>
2022-01-25 11:27:03 -0800

    cleaning

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # NFT Leasing
 
-### Initial Specs
+## Introduction
+The NFT Leasing protocol (NLP) allows owners to lease out their NFTs for a specified time period in exchange for payment. Do your NFTs give you access to exclusive events you can't attend? NLP compensates your for your fomo with ETH payments while temporary borrowers get the chance of flexing flashy blue chips. 
+
+## Development 
+
+### Set up 
+```
+git clone https://github.com/gstenger98/nft-leasing
+git submodule update --init --recursive ## install dependencies
+forge build
+```
+
+### Test 
+
+```
+forge test
+```
+
+### Lint 
+```
+npm install 
+npm run lint 
+```
+
+## Initial Specs
 1. A lender and borrower must both consent into the following contract
 2. The borrower wants to be able to lease a specific NFT for a predefined period of time
 3. Require that the lender owns the NFT
--- src/NftLease.sol
+++ src/NftLease.sol
@@ -1,45 +1,52 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-contract LeaseNFT {
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {PRBMathSD59x18} from "../lib/prb-math/contracts/PRBMathSD59x18.sol";
+
+contract LeaseNFT is ERC721 {
+
+    // Use PRBMathSD59x18 
+    using PRBMathSD59x18 for int256;
 
     // The address of the original owner
-    address payable public lenderAddress;
+    address payable public immutable lenderAddress;
 
     // The address of the tempory borrower
-    address public borrowerAddress;
-
-    // The NFT being leased
-    address public leasedNFT;
+    address public immutable borrowerAddress;
 
     // The expiration time of the lease
-    uint256 public expiry;
+    uint256 public immutable expiry;
 
     // The amount of ETH the borrower must pay the lender in order to lease the specified NFT for the specified period
-    uint256 public costToLease;
+    uint256 public immutable costToLease;
 
     // The amount of additional ETH the lender requires as collateral
-    uint256 public collateral;
+    uint256 public immutable collateral;
 
     // The interest rate the borrower must pay if the expiration is exceeded
-    uint256 public interestRate;
+    uint256 public immutable interestRate;
+
+    // Errors
+    error InsufficientPayment();
+    error FailedToSendEther();
 
     constructor(
         address payable _lenderAddress,
         address payable _borrowerAddress,
-        address _leasedNFT,
+        string memory _name,
+        string memory _symbol,
         uint256 _expiry,
         uint256 _costToLease,
         uint256 _collateral,
         uint256 _interestRate
-    ) {
+    ) ERC721(_name, _symbol) {
         // TODO: Require that the _lenderAddress owns the _leasedNFT
         require(expiry > block.timestamp, "Expiry is before current time");
         // TODO: Require that the _borrowerAddress has more than _costToLease + _collateral
         
         lenderAddress = _lenderAddress;
         borrowerAddress = _borrowerAddress;
-        leasedNFT = _leasedNFT;
         expiry = _expiry;
         costToLease = _costToLease;
         collateral = _collateral;
@@ -52,7 +59,10 @@ contract LeaseNFT {
 
     // Send the initial payment from the borrower to the lender
     function _sendInitialPayment() private {
-
+        (bool sent, ) = msg.sender.call{value: msg.value}("");
+        if (!sent) {
+            revert FailedToSendEther();
+        }
     }
 
     // Store the required collateral in this contract
@@ -70,16 +80,19 @@ contract LeaseNFT {
 
 /*
 Questions:
-    1. Who calls this contract? How do we make sure both parties consent to this agreement?
-    2. What are clearest variable names?
+    1. Are all my functions/variables properly scoped?
+    2. Do I use memory/storage in the correct places?
+    3. Which functions need to be payable?
+    4. Am I using immutable correctly?
+    5. Do I use SafeMath/PRBMathSD59x18 in the correct places?
+    6. What license should I use for this? MIT? GPL-3.0? Unliscence?
+    7. Are there other error conditions I should consider?
+    8. Who calls this contract? How do we make sure both parties consent to this agreement?
+    9. What are clearest variable names?
         a. originalOwner vs lenderAddress?
         b. temporaryOwner vs borrowerAddress?
         c. expiry vs expirationTime?
         d. costToLease vs initialPayment?
-    3. How could we accept other forms of collateral other than ETH?
-    4. How can this be exploited?
-    5. Are all my functions/variables properly scoped?
-    6. Do I use SafeMath in the correct places?
-    7. What license should I use for this? MIT? GPL-3.0? Unliscence?
-    8. Which functions need to be payable?
+    10. How can this be exploited?
+    11. How could we accept other forms of collateral other than ETH?
 */
