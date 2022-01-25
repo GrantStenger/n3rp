a8356c2d12c76edea72f56af702a63f68f600745
Grant Stenger <gstenger98@gmail.com>
2022-01-25 13:39:55 -0800

    request payment implemented

--- src/NftLease.sol
+++ src/NftLease.sol
@@ -31,6 +31,15 @@ abstract contract LeaseNFT is ERC721 {
     // The interest rate the borrower must pay if the expiration is exceeded
     uint256 public immutable interestRate;
 
+    // The time when the contract officially begins
+    uint256 public immutable contractInitializationTime;
+
+    // The amount of collateral left in the contract
+    uint256 public collateralLeft;
+
+    // Mapping containing approvals for calling token transfer
+    // mapping(uint => address) transferApprovals;
+
     // Errors
     error InsufficientPayment();
     error FailedToSendEther();
@@ -46,10 +55,16 @@ abstract contract LeaseNFT is ERC721 {
         uint256 _collateral,
         uint256 _interestRate
     ) ERC721(_name, _symbol) {
-        // TODO: Require that the _lenderAddress owns the _leasedNFT
+        // Require that the _lenderAddress owns the specified NFT
+        require(ownerOf(_tokenId) == _lenderAddress);
+
+        // Require that the _borrowerAddress has more than _costToLease + _collateral
+        require(_borrowerAddress.balance >= _costToLease + _collateral);
+
+        // Require that the expiry is in the future
         require(_expiry > block.timestamp, "Expiry is before current time");
-        // TODO: Require that the _borrowerAddress has more than _costToLease + _collateral
         
+        // Assign our declared variables
         lenderAddress = _lenderAddress;
         borrowerAddress = _borrowerAddress;
         tokenId = _tokenId
@@ -57,10 +72,31 @@ abstract contract LeaseNFT is ERC721 {
         costToLease = _costToLease;
         collateral = _collateral;
         interestRate = _interestRate;
+        interestPerBlock = interestRate / 365 days;
+        
+
+        // I need to be careful calculating this
+        // timeToDepletion = log(collateral/payment, interestPerBlock);
+
+        /*
+        1)  The constructor certifies that the lender owns the NFT and 
+            the borrower has collateral + payment.
+        2a) The borrower sends payment + collateral to the contract.
+        2b) The lender sends the NFT to the contract.
+        3)  If 2a and 2b are satisfied, then the contract sends the 
+            NFT to the borrower and the payment to the lender.
+        4)  If the borrower sends the NFT back to the contract before 
+            the expiry, the contract sends the collateral back to the 
+            borrower and the NFT to the lender.
+        5)  If the current time is past the expiry and the NFT still has
+            not been returned to the contract, the contract pays the 
+            lender 
+        */
+
 
         _sendInitialPayment();
         _storeCollateral();
-        _transfer();
+        _transfer(borrowerAddress, lenderAddress, tokenId);
     }
 
     // Send the initial payment from the borrower to the lender
@@ -77,21 +113,42 @@ abstract contract LeaseNFT is ERC721 {
     }
 
     function _transfer(address _from, address _to, uint256 _tokenId) private {
-        emit Transfer(_from, _to, _tokenId);
+        emit Transfer(_from, _to, tokenId);
     }
 
     // Once the collateral is in the contract, the lender approves the tranfer
     function approveTransfer(address _to, uint256 _tokenId) external payable {
         // Note: this function must be called by the lender
-        transferFrom(msg.sender, _to, _tokenId);
+        require(msg.sender == _owners[_tokenId]);
+        _tokenApprovals[_tokenId] = _to;
+
     }
 
     // Once the lender has approved the transfer, transfer the NFT to the borrower
-    function transferOwnership(address _from, uint256 tokenId) external payable {
-        // Note: this function is called by the borrower
-        transferFrom(_from, msg.sender, _tokenId);
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(msg.sender == _owners[_tokenId] || msg.sender == _tokenApprovals[_tokenId]);
+        _transfer(_from, msg.sender, _tokenId);
     }
 
+    // If the expiry has passed and the lender has not received thier NFT back, they can request payment
+    // (Techincally anyone should be able to call this on the behalf of the lender)
+    function requestCollateral() external payable {
+
+        // Calculate the amount that the lender can be paid at this time
+        payableCollateral = (block.timestamp - contractInitializationTime) / timeToDepletion - collateralLeft;
+
+        // Send the lender their payment
+        (bool sent, ) = lenderAddress.call{value: payableCollateral}("");
+
+        // Check if the payment was sent
+        if (!sent) {
+            // Throw error
+            revert FailedToSendEther();
+        } else {
+            // Update the collateral left
+            collateralLeft -= payableCollateral;
+        }
+    }
 }
 
 
