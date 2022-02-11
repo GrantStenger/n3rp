12618a31ee952272ee7506eb950246f916622686
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-02-11 17:49:12 -0500

    clean up vite config to disable ssr due to use with vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite"
-import reactRefresh from "@vitejs/plugin-react-refresh"
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     minify: false,
+    rollupOptions: { input: "index.html" },
   },
-})
+});
