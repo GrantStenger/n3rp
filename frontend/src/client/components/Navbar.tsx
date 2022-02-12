4bf1d3cde2d01a245f202035a68a478e4dab150b
Grant Stenger <gstenger98@gmail.com>
2022-02-12 12:30:43 -0800

    organizing navbar

--- frontend/src/client/components/Navbar.tsx
+++ frontend/src/client/components/Navbar.tsx
@@ -6,18 +6,18 @@ export const Navbar = () => {
   return (
     <header className={"justify-center items-center bg-slate-400 w-full"}>
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <div className="">
           <img src={nerpLogo} className="h-60 inline" alt="logo" />
         </div>
-        <div className="justify-center items-center flex">
-          <div className="text-white font-bold py-2 px-4">
+        <div className="flex">
+          <div className="text-white hover:text-slate-100 font-bold py-2 px-4">
             <a href="">Marketplace</a>
           </div>
-          <div className="text-white font-bold py-2 px-4">
+          <div className="text-white hover:text-slate-100 font-bold py-2 px-4">
             <a href="">Create Contract</a>
           </div>
-          <div className="text-white font-bold py-2 pl-4 pr-10">
-            <a href="">About</a>
+          <div className="text-white hover:text-slate-100 font-bold py-2 pl-4 pr-10">
+            <a href="https://github.com/gstenger98/N3RP/blob/master/README.md">About</a>
           </div>
           <button className="float-right bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
             Connect Wallet
