72383896f6f18c1099219c85655b3ec21cccba8f
Cameron Kelley <cameron@kelley.vc>
2022-02-20 10:55:01 -0600

    Add nft ownership lookup

--- frontend/src/client/components/ContractCreator.tsx
+++ frontend/src/client/components/ContractCreator.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useAccount, useConnect } from "wagmi";
-import { Formik, Field, useFormik, useFormikContext } from 'formik';
+import { Formik, Field, useFormikContext } from 'formik';
 import { getNFTs, getNFTMetadata } from "../lib/web3";
 import { GetNftMetadataResponse } from "@alch/alchemy-web3";
 import Spinner from "./Spinner";
+
+import { erc721ABI, useContractRead } from "wagmi";
+import ethers from "ethers";
 
 type Mode = ("LEND" | "BORROW") | null;
 
@@ -52,21 +55,25 @@ const Input = ({
   )
 }
 
-const NFTDisplay = ({ metadata }: { metadata: GetNftMetadataResponse }) => {
+type NFTMetadata = GetNftMetadataResponse & {
+  owner: string;
+}
+
+const NFTDisplay = ({ metadata }: { metadata: NFTMetadata }) => {
   const img = metadata?.media && metadata.media.length > 0 
     ? metadata.media[0].uri?.gateway 
     : null;
   return (
     <div className="flex flex-row flex-wrap">
-      {img && <img src={img} className="w-32 h-32 mr-4" />}
+      {img && <img src={img} className="w-32 h-32 mr-4 mb-4" />}
       <div className="flex-1 space-y-2">
-        <h1 className="text-lg font-bold">{metadata.title}</h1>
-        {/* {metadata.metadata?.description && (
-          <p>{metadata.metadata.description}</p>
-        )} */}
+        <div>
+          <h1 className="text-lg font-bold">{metadata.title}</h1>
+          <div className="text-sm">Owned by <a href={`https://etherscan.io/address/${metadata.owner}`} target="_blank" rel="noreferrer" className="text-blue-500">{metadata.owner.substring(0,6)}</a></div>
+        </div>
         {metadata.metadata?.attributes?.map(attribute => (
-          <div className="inline-block bg-slate-100 px-2 py-1 rounded-xl text-xs mr-2 mb-2">
-            <span className="font-bold">{attribute.trait_type}</span>: {attribute.value}
+          <div key={attribute.trait_type} className="inline-block bg-slate-100 px-2 py-1 rounded-xl text-xs mr-2 mb-2 max-w-full">
+            <span className="font-bold">{attribute.trait_type}</span>: <span className="break-all">{attribute.value}</span>
           </div>
         ))}
       </div>
@@ -77,19 +84,44 @@ const NFTDisplay = ({ metadata }: { metadata: GetNftMetadataResponse }) => {
 const SelectNFTByAddress = () => {
   const { values, setFieldValue } = useFormikContext<any>(); // fixme
   const [loadingMetadata, setLoadingMetadata] = useState(false);
+  const [_, read] = useContractRead(
+    {
+      addressOrName: values.contractAddr,
+      contractInterface: erc721ABI,
+    },
+    // ERC721 function name to get owner of a token by tokenId.
+    'ownerOf',
+    {
+      // Only run this read with the `read` function.
+      skip: true
+    }
+  );
   
   const lookupMetadata = useMemo(() => {
-    return () => {
+    return async () => {
       setLoadingMetadata(true);
-      getNFTMetadata(
-        values.contractAddr as string,
-        values.tokenId as string,
-      ).then((metadata) => {
-        console.log("metadata", metadata)
-        setFieldValue("_metadata", metadata)
-      }).finally(() => {
-        setLoadingMetadata(false);
+      
+      // In parallel, grab NFT metadata and current owner.
+      // FIXME: This assumes that the the NFT implements the
+      //    ERC-721 interface. Can we always make this assumption?
+      //    How should we fall back otherwise?
+      const results = await Promise.all([
+        getNFTMetadata(
+          values.contractAddr as string,
+          values.tokenId as string,
+        ),
+        read({
+          args: [values.tokenId]
+        })
+      ])
+
+      setFieldValue("_metadata", {
+        // Unpack metadata
+        ...results[0],
+        // Add a custom `owner` k/v
+        owner: results[1]?.data || null
       })
+      setLoadingMetadata(false);
     }
   }, [values.contractAddr, values.tokenId])
 
@@ -99,19 +131,27 @@ const SelectNFTByAddress = () => {
     };
   }, [])
 
+  const confirm = useMemo(() => {
+    return () => {
+      setFieldValue("nftConfirmed", true);
+    };
+  }, [])
+
   return values._metadata ? (
     <>
       <NFTDisplay
         metadata={values._metadata as GetNftMetadataResponse}
       />
-      <div className="space-x-2">
-        <button className={`_button`} onClick={reset}>
-          Borrow this NFT
-        </button>
-        <button className={`_buttonBasic`} onClick={reset}>
-          Cancel
-        </button>
-      </div>
+      {!values.nftConfirmed ? (
+        <div className="space-x-2">
+          <button className={`_button`} onClick={confirm}>
+            Borrow this NFT
+          </button>
+          <button className={`_buttonBasic`} onClick={reset}>
+            Cancel
+          </button>
+        </div>
+      ) : null}
     </>
   ) : loadingMetadata ? (
     <div className="pt-4 flex flex-row items-center justify-center">
@@ -151,9 +191,11 @@ export const ContractCreator = () => {
   return (
     <Formik
       initialValues={{
-        mode: null,
+        mode: "BORROW",
         contractAddr: '0xa755a670aaf1fecef2bea56115e65e03f7722a79',
-        tokenId: '',
+        tokenId: '0',
+        _metadata: null,
+        nftConfirmed: true,
       }}
       onSubmit={() => { console.log('Submitted'); }}
     >
@@ -218,10 +260,17 @@ export const ContractCreator = () => {
             </div>
           ) : null}
           {data.connected && f.values.mode === "BORROW" ? (
-            <div className="_card max-w-120 space-y-4">
-              <h2 className="text-xl font-bold">Select NFT</h2>
-              <SelectNFTByAddress />
-            </div>
+            <>
+              <div className="_card max-w-120 space-y-4">
+                <h2 className="text-xl font-bold">Select NFT</h2>
+                <SelectNFTByAddress />
+              </div>
+              {f.values.nftConfirmed === true ? (
+                <div className="_card max-w-120 space-y-4">
+                  
+                </div>
+              ) : null}
+            </>
           ) : null}
         </div>
       )}
