a8ed8f2e347ff5c3c4f6e279252709b16bacec2b
Cyrus Cowley <cyficowley@gmail.com>
2022-04-29 14:58:25 -0700

    filtering explore

--- frontend/src/client/components/ExploreRentals.tsx
+++ frontend/src/client/components/ExploreRentals.tsx
@@ -1,44 +1,170 @@
-import React from "react";
+import React, { useState, forwardRef } from "react";
 import { NftWithMetadata, AvaliabilityStatus } from "../../../types/nftTypes.js";
 import { ListingPanel } from "./ListingPanel";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import { diffieHellman } from "crypto";
 
 export const ExploreRentals = () => {
-
   const nft: NftWithMetadata = {
     nft: {
       listing: {
         name: "Jelly bean",
         description: "it has a crown and stuff",
-        datesForRent: [],
+        datesForRent: [
+          {
+            startDate: new Date(),
+            endDate: new Date(2022, 6, 1),
+          },
+        ],
         pricePerDay: 1,
         collateral: 2,
       },
       nftSpecification: {
         nftCollection: "0xa755a670aaf1fecef2bea56115e65e03f7722a79",
         nftId: "0",
-      }
+      },
     },
     image: "https://ipfs.io/ipfs/QmNj6UmToxxnJFqW13GkG3NHX1FXtHsHwAbuB8uQasPZUm",
     avaliability: {
       status: AvaliabilityStatus.Avaliabile,
     },
-    attributes: [{ traitType: "asdf", value: "asdf2" }]
+    attributes: [{ traitType: "asdf", value: "asdf2" }],
   };
 
-  let panels = []
-
+  let nfts = [];
   for (let i = 0; i < 8; i++) {
-    panels.push(<ListingPanel nft={nft} />);
+    nfts.push(nft);
   }
 
+  const [dateFilterVisible, setDateFilterVisible] = useState(false);
+
+  const [dateRange, setDateRange] = useState<[null | Date, null | Date]>([null, null]);
+  const [startDate, endDate] = dateRange;
+
+  const [costFilterVisible, setCostFilterVisible] = useState(false);
+  const [startCost, setStartCost] = useState<null | number>(null);
+  const [endCost, setEndCost] = useState<null | number>(null);
+
+  const renderDateFilter = () => {
+    let text = "Filter by date";
+
+    if (startDate) {
+      text = "Filtering from " + startDate.toLocaleDateString() + " - ";
+      if (endDate) {
+        text += endDate.toLocaleDateString();
+      }
+    }
+
+    return (
+      <div className="relative">
+        <button
+          className="border-2 border-black bg-slate-200  py-1 px-2 text-sm"
+          onClick={() => setDateFilterVisible(!dateFilterVisible)}
+        >
+          {text}
+        </button>
+        {startDate && endDate && (
+          <button
+            className="border-2 border-black bg-slate-200  py-1 px-2 text-sm ml-2"
+            onClick={() => setDateRange([null, null])}
+          >
+            Clear dates
+          </button>
+        )}
+        {dateFilterVisible && (
+          <div className="absolute">
+            <DatePicker
+              selectsRange={true}
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(update: any) => {
+                setDateRange(update);
+                const [start, end] = update;
+                if (start && end) {
+                  setDateFilterVisible(false);
+                }
+              }}
+              minDate={new Date()}
+              showDisabledMonthNavigation
+              inline
+            />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const renderCostFilter = () => {
+    let text = "Filter by cost";
+
+    if (startCost && !endCost) {
+      text = "Greater than ETH(" + startCost + ")";
+    } else if (!startCost && endCost) {
+      text = "Less than ETH(" + endCost + ")";
+    } else if (startCost && endCost) {
+      text = "Between ETH(" + startCost + ") - ETH(" + endCost + ")";
+    }
+    return (
+      <div className="relative">
+        <button
+          className="border-2 border-black bg-slate-200  py-1 px-2 text-sm"
+          onClick={() => setCostFilterVisible(!costFilterVisible)}
+        >
+          {text}
+        </button>
+        {costFilterVisible && (
+          <div className="absolute">
+            <input
+              type="number"
+              placeholder="At least"
+              value={startCost ? startCost : ""}
+              onChange={event => setStartCost(event.target.valueAsNumber)}
+            />
+            <input
+              type="number"
+              placeholder="At most"
+              value={endCost ? endCost : ""}
+              onChange={event => setEndCost(event.target.valueAsNumber)}
+            />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  if (startDate && endDate) {
+    nfts = nfts.filter(nft => {
+      return nft.nft.listing.datesForRent.some(avaliableDate => {
+        return avaliableDate.startDate <= startDate && avaliableDate.endDate >= endDate;
+      });
+    });
+  }
+  if (startCost) {
+    nfts = nfts.filter(nft => {
+      return nft.nft.listing.pricePerDay >= startCost;
+    });
+  }
+  if (endCost) {
+    nfts = nfts.filter(nft => {
+      return nft.nft.listing.pricePerDay <= endCost;
+    });
+  }
 
   return (
     <>
-      <p>Exploring the rentals</p>
       <div className="container">
-        <div className="grid grid-cols-4 gap-4 w-full">{panels}</div>
+        <div className="flex pb-3">
+          <div className="mr-4">{renderDateFilter()}</div>
+          <div>{renderCostFilter()}</div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 w-full">
+          {nfts.map(nft => (
+            <ListingPanel nft={nft} />
+          ))}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
--- frontend/src/client/components/ListingPanel.tsx
+++ frontend/src/client/components/ListingPanel.tsx
@@ -1,45 +1,43 @@
 import React from "react";
 import { NftWithMetadata, Avaliability, AvaliabilityStatus } from "../../../types/nftTypes.js";
 
-
 export const ListingPanel = ({ nft }: { nft: NftWithMetadata }) => {
-
   const renderAvaliability = (availability: Avaliability) => {
-
     let statusText;
     switch (availability.status) {
       case AvaliabilityStatus.Avaliabile:
-        statusText = 'Avaliable Now!';
+        statusText = "Avaliable Now!";
         break;
       case AvaliabilityStatus.Rented:
-        statusText = 'Currently rented, available on ' + availability.AvaliabiltyDate + "!";
+        statusText = "Currently rented, available on " + availability.AvaliabiltyDate + "!";
         break;
       case AvaliabilityStatus.Unavaliabile:
-        statusText = 'Currently unavailable';
+        statusText = "Currently unavailable";
         break;
       case AvaliabilityStatus.Upcoming:
-        statusText = 'Will be available on ' + availability.AvaliabiltyDate + "!";
+        statusText = "Will be available on " + availability.AvaliabiltyDate + "!";
         break;
     }
 
     let classes = "border-2 border-black bg-slate-200  py-1 px-2 text-sm";
     if (availability.status === AvaliabilityStatus.Avaliabile) {
       classes += " hover:bg-slate-400 cursor-pointer";
-    }
-    else {
+    } else {
       classes += " cursor-default";
     }
 
-    return <button className={classes}>{statusText}</button>
-  }
+    return <button className={classes}>{statusText}</button>;
+  };
 
   return (
     <div className="bg-white p-3 shadow">
-      <div className="imageContainer w-full overflow-auto bg-black bg-center bg-cover" style={{ backgroundImage: `url(${nft.image})` }}>
-        <div className="w-full" style={{ marginTop: "100%", }} >
-        </div>
+      <div
+        className="imageContainer w-full overflow-auto bg-black bg-center bg-cover"
+        style={{ backgroundImage: `url(${nft.image})` }}
+      >
+        <div className="w-full" style={{ marginTop: "100%" }}></div>
       </div>
-      <div >
+      <div>
         <div className="w-full flex items-center pb-2">
           <h1 className="text-lg font-bold">{nft.nft.listing.name}</h1>
           <div className="flex-grow pl-1"></div>
@@ -51,10 +49,8 @@ export const ListingPanel = ({ nft }: { nft: NftWithMetadata }) => {
           </div>
         ))}
         <p>{nft.nft.listing.description}</p>
-        <div className="pt-2">
-          {renderAvaliability(nft.avaliability)}
-        </div>
+        <div className="pt-2">{renderAvaliability(nft.avaliability)}</div>
       </div>
     </div>
-  )
-}
+  );
+};
